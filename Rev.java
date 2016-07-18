1135944d544cb97a5cfcd66a517fea9460f7c2ee
731613205025 <maniharish95@gmail.com>
2016-07-18 13:38:42 +0530

    Add files via upload

---
+++ Rev.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rev;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Student
+ */
+public class Rev {
+
+    
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a sentence: ");
+        String input = scanner.nextLine();
+        String result = "";
+        char firstChar = input.charAt(0);
+        result = result + Character.toUpperCase(firstChar);
+        for (int i = 1; i < input.length(); i++) {
+            char currentChar = input.charAt(i);
+            char previousChar = input.charAt(i - 1);
+            if (previousChar == ' ') {
+                result = result + Character.toUpperCase(currentChar);
+            } else {
+                result = result + currentChar;
+            }
+        }
+        System.out.println(result);
+    }
+}
+    
+
